5cb334d8d4c00def3de85ef2248bb3019280489c
mfgomezb <mgomezb89@gmail.com>
2018-10-16 11:04:06 +0200

    private rooms working

--- server/io.js
+++ server/io.js
@@ -7,12 +7,19 @@ module.exports = (io) => {
     io.on('connection', function(socket){
         console.log(`A user connected with id: ${socket.id}`);
         console.log(socket.handshake.headers.cookie);    
-//        socket.emit('message',{msg:"Holafromserverrr", timestamp: Date.now()});
+    //    socket.emit('message',{msg:"Holafromserverrr", timestamp: Date.now()});
+
+        socket.on('subscribe', function(room) {
+            console.log('joining room', room);
+            socket.join(room)
+        })
         
-        socket.on('message', data => {
+        socket.on('send message', data => {
             console.log("Received message from client");
             console.log(data);
-            socket.broadcast.emit('message', data);
+            socket.broadcast.emit('conversation private post', {
+                message: data.message
+            });
         })
     });
 
